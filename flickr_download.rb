3c4e6ca40048d84dcf71bed5d74d6af676609f92
jssmk <jussmaki@fastmail.fm>
2016-09-10 18:22:55 +0000

    Ruby program and example settings file

---
+++ flickr_download.rb
@@ -0,0 +1,60 @@
+#
+#  Download photos belonging to some group
+#        jssmk @ Helsinki Hacklab
+#
+
+# Start with editing your locals.rb file, use locals_default.rb as a template
+require_relative 'locals'
+
+local_pic_list = Dir[PIC_path+"**{.JPG,.jpg}"]
+
+
+require 'flickraw'
+#require 'exifr'
+require 'logger'
+require 'open-uri'
+
+# Logfile
+$mylog = Logger.new(LOG_path+'flickr_download_log.txt', 10, 1024000)
+$mylog.info("Start ---->")
+
+FlickRaw.api_key = MY_api_key
+FlickRaw.shared_secret = MY_shared_secret
+
+
+# List all photos in the group
+$group_pic_list = flickr.groups.pools.getPhotos api_key: FlickRaw.api_key, group_id: MY_group_id
+$mylog.info("Number of pics to download "+$group_pic_list.length.to_s)
+
+
+for pic in $group_pic_list
+  
+  if local_pic_list.include?(PIC_path+pic.id+'.jpg')
+    # This pic is already downloaded
+  else
+
+    info = flickr.photos.getInfo api_key: MY_api_key, photo_id: pic.id
+    $mylog.info("getting info for pic: " + pic.id)
+  
+    print("Downloading pic: " + info.title + "\n")
+
+    begin
+      sizes = flickr.photos.getSizes api_key: MY_api_key, photo_id: pic.id
+    rescue FlickRaw::FailedResponse => e
+      $mylog.error("getting sizes for pic " + pic.id + " failed")
+      exit
+    end
+    
+    org_size = sizes.size.select {|s| s.label == 'Original'}[0]
+     
+    # Start downloading
+    
+    open(PIC_path + pic.id + '.jpg', 'wb') do |file|
+      file << open(org_size.source).read
+    end
+    
+    $mylog.info("downloaded pic: "+pic.id)
+  end
+end
+
+$mylog.info("----> End")
---
+++ locals-default.rb
@@ -0,0 +1,19 @@
+
+#
+# Set your local options using this file
+#
+
+# Select which group pics to download
+
+MY_group_id = "000000@N22"
+
+
+# Local paths
+
+PIC_path = "/home/pi/Pictures/"
+
+LOG_path = "/home/pi/Documents/"
+
+# Flickr keys
+MY_api_key = "00000000000000000000000000000000"
+MY_shared_secret="0000000000000000"
